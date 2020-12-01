5f567d8ab9430e0c933877110d3764fcb2d16114
eveline-hutter <eveline.hutter@students.ffhs.ch>
2020-12-01 12:30:34 +0100

    solved day1

---
+++ src/day1/R1.java
@@ -0,0 +1,38 @@
+package day1;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.io.*;
+
+public class R1 {
+
+    public int getPair() throws IOException {
+        Path path = Paths.get("D:\\Documents\\GitHub\\advent-of-code-2020\\data\\day1input.txt");
+        List<String> input = Files.readAllLines(path);
+        // sort data
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = i + 1; j < input.size(); j++) {
+                int first = Integer.parseInt(input.get(i));
+                int second = Integer.parseInt(input.get(j));
+                if ( (first + second) == 2020) {
+                    System.out.println("Pair found! First: " + first + " (" + i + ") | Second: " + second + " (" + j + ")");
+                    return first * second;
+                }
+            }
+        }
+        return -1;
+    }    
+
+    public static void main(String[] args) {
+        R1 r1 = new R1();
+        int solution = 0;
+        try {
+            solution = r1.getPair();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println(solution);
+    }
+}
---
+++ src/day1/R2.java
@@ -0,0 +1,42 @@
+package day1;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class R2 {
+
+    public int getTriplet() throws IOException {
+        Path path = Paths.get("D:\\Documents\\GitHub\\advent-of-code-2020\\data\\day1input.txt");
+        List<String> input = Files.readAllLines(path);
+        // sort data
+        for (int i = 0; i < input.size(); i++) {
+            for (int j = i + 1; j < input.size(); j++) {
+                for (int k = j + 1; k < input.size(); k++) {
+                    int first = Integer.parseInt(input.get(i));
+                    int second = Integer.parseInt(input.get(j));
+                    int third = Integer.parseInt(input.get(k));
+                    if ( (first + second + third) == 2020) {
+                        System.out.println("Triplet found! First: " + first + " (" + i + ") | Second: "
+                                + second + " (" + j + ") | Third: " + third + " (" + k + ")");
+                        return first * second * third;
+                    }
+                }                
+            }
+        }
+        return -1;
+    }    
+
+    public static void main(String[] args) {
+        R2 r2 = new R2();
+        int solution = 0;
+        try {
+            solution = r2.getTriplet();
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        System.out.println(solution);
+    }
+}
